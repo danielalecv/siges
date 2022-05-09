f6200542d61374e38137d15dd50ea68401309a98
daniel <daniel.alecv5@gmail.com>
2022-05-09 15:53:39 -0500

    Agregar archivos de proyecto.

---
+++ siges/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using siges.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace siges.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Console.WriteLine("Home page");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            Console.WriteLine("Privacy page");
+            ViewData["Message"] = "Privacy page";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
